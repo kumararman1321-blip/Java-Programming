65bee732d247f9cd56a662e678f164b48221de71
kumararman1321-blip <kumararman1321@gmail.com>
2026-08-24 17:29:41 +0530

    Replace dynamic input with static 2D array initialization
    Removed user input for array size and elements, replacing it with a static 2D array initialization. Updated the output to display the static 2D array directly.

--- twoDArray.java
+++ twoDArray.java
@@ -1,24 +1,21 @@
-import java.util.Scanner;
-
 public class twoDArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter array size: ");
-        int n = sc.nextInt();
+        // Declare and initialize a 2D array
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
 
-        int[] arr = new int[n];
+        // Print the 2D array
+        System.out.println("2D Array:");
 
-        System.out.println("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
-
-        System.out.println("Array elements:");
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
-        }
-
-        sc.close();
     }
 }
